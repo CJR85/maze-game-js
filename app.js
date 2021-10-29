d03982b2e7726cb56b3c6ea80117872a0b0e2fb7
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-29 19:22:25 +0100

    Add svg maze classes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Svg mazes
+const levelOne = document.querySelector('.level-one'),
+  levelTwo = document.querySelector('.level-two');
+
 const collisionCheck = (value) => {
   if (value === 'maze-border') alert('You lost the game...try again.');
 };
