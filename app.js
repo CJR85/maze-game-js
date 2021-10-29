f35187b821910ed3df516d786d3b5f12ceecc4b7
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-29 19:42:37 +0100

    Create addEventListener to nextButton

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const nextButton = document.querySelector('.next-button');
 // Svg mazes
 const levelOne = document.querySelector('.level-one'),
   levelTwo = document.querySelector('.level-two');
+// UI Messages
+const uiLevel = document.querySelector('.ui-level'),
+  uiMessage = document.querySelector('.ui-message');
 
 const collisionCheck = (value) => {
   if (value === 'maze-border') alert('You lost the game...try again.');
@@ -15,4 +18,13 @@ const collisionCheck = (value) => {
 window.addEventListener('mousemove', (e) => {
   let check = e.target.classList.value;
   collisionCheck(check);
+});
+
+nextButton.addEventListener('click', () => {
+  levelOne.style.display = 'none';
+  levelTwo.style.display = 'block';
+  nextButton.style.opacity = 0;
+  nextButton.style.pointerEvents = 'none';
+  uiLevel.textContent = 'Level 2';
+  uiMessage.textContent = 'One more to go...';
 });
