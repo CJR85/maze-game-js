b5cedbd6b7cb1f889e8abcc5045ed2d1a02af5f5
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-29 19:33:46 +0100

    Create function for finish and next button

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const nextButton = document.querySelector('.next-button');
 // Svg mazes
 const levelOne = document.querySelector('.level-one'),
   levelTwo = document.querySelector('.level-two');
 
 const collisionCheck = (value) => {
   if (value === 'maze-border') alert('You lost the game...try again.');
+  if (value === 'finish') {
+    nextButton.style.opacity = 1;
+    nextButton.style.pointerEvents = 'all';
+    levelOne.style.pointerEvents = 'none';
+  }
 };
 
 window.addEventListener('mousemove', (e) => {
